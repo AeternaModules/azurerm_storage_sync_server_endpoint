88c32aed52b167b13dd12690c15d9dc234c5a6a7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:31:20 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "storage_sync_server_endpoints" {
+  description = "All storage_sync_server_endpoint resources"
+  value       = azurerm_storage_sync_server_endpoint.storage_sync_server_endpoints
+}
+
